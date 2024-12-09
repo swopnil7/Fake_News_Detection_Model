c213160220a0b3226d368500ab3901233328fbc2
swopnil7 <report2swopnil@gmail.com>
2024-12-09 19:13:52 +0545

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -5,7 +5,7 @@ import numpy as np
 true = pd.read_csv('data/raw/True.csv')
 fake = pd.read_csv('data/raw/Fake.csv')
 
-# Display the first few rows of each dataset (optional for .py)
+# Display the first few rows of each dataset
 print(true.head())
 print(fake.head())
 
@@ -13,14 +13,14 @@ print(fake.head())
 true['label'] = 1
 fake['label'] = 0
 
-# Display the updated dataframes (optional for .py)
+# Display the updated dataframes
 print(true.head())
 print(fake.head())
 
 # Concatenate the datasets
 news = pd.concat([fake, true], axis=0)
 
-# Display the concatenated dataframe (optional for .py)
+# Display the concatenated dataframe
 print(news.head())
 
 # Check for null values
@@ -29,19 +29,19 @@ print(news.isnull().sum())
 # Drop unnecessary columns
 news = news.drop(['title', 'subject', 'date'], axis=1)
 
-# Display the updated dataframe (optional for .py)
+# Display the updated dataframe
 print(news.head())
 
 # Shuffle the dataset
 news = news.sample(frac=1)
 
-# Display the shuffled dataframe (optional for .py)
+# Display the shuffled dataframe
 print(news.head())
 
 # Reset index
 news.reset_index(inplace=True)
 
-# Display the dataframe after resetting the index (optional for .py)
+# Display the dataframe after resetting the index
 print(news.head())
 
 # Drop the index column
